c3bd0191c591343be5ebcc9d85a80d0fa1396a11
Jason Dancocks <jason.dancocks@gmail.com>
2017-08-30 20:49:26 +0100

    complete flight selection

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,10 @@
 class FlightsController < ApplicationController
-  def index
-  	@airport_options = Airport.all.map { |a| [a.code, a.id]}
-  	@flight_dates = Flight.available_dates
-  	@available_flights = Flight.available_flights
-  end
+	def index
+		@airport_options = Airport.all.map { |a| [a.code, a.id]}
+		@flight_dates = Flight.available_dates
+
+		unless params[:commit].nil?
+			@available_flights = Flight.available_flights(params)
+		end
+	end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -7,17 +7,22 @@ class Flight < ApplicationRecord
 		flights.map {|f| f.start_date_formatted}.uniq
 	end
 
+	def self.available_flights(params)
+		parsed_date = parse_date(params[:start_date])
+		where(origin_airport_id:params[:origin_airport_id], destination_airport_id: params[:destination_airport_id], start_date: parsed_date)
+	end
+
+	def self.parse_date(date)
+		date_time = date.to_date
+		date_time.beginning_of_day..date_time.end_of_day
+	end
+	
 	def start_date_formatted
 		start_date.strftime("%d/%m/%Y")
 	end
 
-	def self.available_flights(origin,destination,date)
-		where(origin_airport_id:origin, destination_airport_id: destination, date: parse_date(date))
-	end
-
-	def parse_date(date)
-		datetime = date.to_date
-		datetime.beginning_of_day..datetime.end_of_day
+	def flight_time
+		start_date.strftime("%H : %M")
 	end
 
 end
