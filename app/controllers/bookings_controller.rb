db75d572ce19e69368eb9f67dc185c827a8c2b60
Jason Dancocks <jason.dancocks@gmail.com>
2017-08-31 16:45:14 +0100

    complete booking process

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -11,9 +11,25 @@ class BookingsController < ApplicationController
 
 
 	def create
+		@booking = Booking.new(booking_params)
+		@flight = Flight.find(params[:booking][:flight_id])
+		if @booking.save!
+			redirect_to @booking
+			flash[:success] = "Booking successful"
+		else
+			flash[:warning] = "Booking failed"
+			render "new"
+		end
 	end
 
 	def show
+		@booking = Booking.find(params[:id])
+	end
+
+	private
+
+	def booking_params
+		params.require(:booking).permit(:flight_id, passengers_attributes: %i[id name email])
 	end
 
 end
